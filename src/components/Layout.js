26807795cd4bc86757e87461649e65ee7acf2a7c
TheBinaryGuy <heet_mehta@hotmail.com>
2018-10-09 19:43:21 +0530

    Meta Tags

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -4,14 +4,23 @@ export default (props) => {
     return (
         <div>
             <Head>
+                <title>TheBinaryBlog</title>
+                <meta name="description" content="A blog for those who can count from 0 to 1" />
+
+                <meta itemprop="name" content="TheBinaryBlog" />
+                <meta itemprop="description" content="A blog for those who can count from 0 to 1" />
+                <meta itemprop="image" content="https://i.imgur.com/u6fGNkM.jpg" />
+
+                <meta property="og:url" content="https://blogengine.thebinaryguy.me" />
+                <meta property="og:type" content="website" />
                 <meta property="og:title" content="TheBinaryBlog" />
                 <meta property="og:description" content="A blog for those who can count from 0 to 1" />
-                <meta property="og:url" content="https://blogengine.thebinaryguy.me/" />
-                <meta name="Description" content="A blog for those who can only count from 0 to 1" />
-                <meta name="Author" content="TheBinaryGuy" />
-                <meta charset="utf-8" />
-                <meta name="Robots" content="index, follow" />
-                <title>Blog Engine</title>
+                <meta property="og:image" content="https://i.imgur.com/u6fGNkM.jpg" />
+
+                <meta name="twitter:card" content="summary_large_image" />
+                <meta name="twitter:title" content="TheBinaryBlog" />
+                <meta name="twitter:description" content="A blog for those who can count from 0 to 1" />
+                <meta name="twitter:image" content="https://i.imgur.com/u6fGNkM.jpg" />
 
             </Head>
             {props.children}
